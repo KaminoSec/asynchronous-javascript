747dfd062758a63edb21e78c383d81c764c6ac09
ozark-dev <jason@ozarkdev.com>
2023-01-29 23:59:52 -0600

    through promises

--- script.js
+++ script.js
@@ -29,40 +29,21 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbor = function (country) {
-  //AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    // console.log(Object.values(data.currencies)[0].name);
+      if (!neighbor) return;
 
-    // Render country 1
-    renderCountry(data);
-
-    // Get neighbor country 2
-    const [neighbor] = data.borders;
-
-    if (!neighbor) return;
-
-    //AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
-    request2.send();
-
-    request2.addEventListener('load', function () {
-      // Render country 2
-      const [data2] = JSON.parse(this.responseText);
-      console.log(data2);
-      renderCountry(data2, 'neighbour');
-    });
-  });
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+    })
+    .then(response => response.json())
+    // .then(data => console.log(data[0]));
+    .then(data => renderCountry(data[0], 'neighbour'));
 };
 
-getCountryAndNeighbor('usa');
-
-const request = fetch('https://restcountries.com/v3.1/name/portugal');
-console.log(request);
+getCountryData('usa');
